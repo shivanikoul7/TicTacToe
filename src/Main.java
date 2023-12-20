a377d367f4f84efa0d6b891f3d94d4bacfdf7a80
shivanikoul7 <shivanikoul701@gmail.com>
2023-12-20 17:54:25 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    private Pane createConetnt() {
+        Pane root = new Pane();
+        return root;
+    }
+
+    @Override
+    public void start(Stage start) {
+        return super.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
